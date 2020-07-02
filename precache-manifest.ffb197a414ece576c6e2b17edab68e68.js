cdc0c08529cfa0f9dce841959e974698aa19c69c
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-07-02 18:22:50 +1000

    Updates

---
+++ precache-manifest.ffb197a414ece576c6e2b17edab68e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be6ab5c61da23be05cae7026ddf0040",
+    "url": "/Employee-directory/index.html"
+  },
+  {
+    "revision": "70ad6f9286b3d9e3dc27",
+    "url": "/Employee-directory/static/css/main.d7e967a7.chunk.css"
+  },
+  {
+    "revision": "6d70114a950b3cf2f693",
+    "url": "/Employee-directory/static/js/2.0d7d5a4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-directory/static/js/2.0d7d5a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ad6f9286b3d9e3dc27",
+    "url": "/Employee-directory/static/js/main.f1d02a3a.chunk.js"
+  },
+  {
+    "revision": "d6e172f98e8882a6f98f",
+    "url": "/Employee-directory/static/js/runtime-main.47436d9b.js"
+  }
+]);
